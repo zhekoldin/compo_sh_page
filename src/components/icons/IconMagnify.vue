9f6dfd5b923ca75195324f45c97800daf20b7ae2
Roman Zhekoldin <zhekoldin@bk.ru>
2024-03-28 22:51:06 +0500

    feat: Добавил иконки инпута

---
+++ src/components/icons/IconMagnify.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "IconMagnify",
+}
+</script>
+
+<template>
+  <mask id="mask0_4708_560" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M5 11C5 7.691 7.691 5 11 5C14.309 5 17 7.691 17 11C17 14.309 14.309 17 11 17C7.691 17 5 14.309 5 11ZM20.707 19.293L17.312 15.897C18.365 14.543 19 12.846 19 11C19 6.589 15.411 3 11 3C6.589 3 3 6.589 3 11C3 15.411 6.589 19 11 19C12.846 19 14.543 18.365 15.897 17.312L19.293 20.707C19.488 20.902 19.744 21 20 21C20.256 21 20.512 20.902 20.707 20.707C21.098 20.316 21.098 19.684 20.707 19.293Z" fill="#071435"/>
+  </mask>
+  <g mask="url(#mask0_4708_560)">
+    <rect width="24" height="24" fill="#050D23"/>
+  </g>
+</template>
+
+<style scoped>
+
+</style>
